25ae328a9fab13591eb49a9f54f3a6a94c3c6a4f
alwierdi <alwi.erdi4@gmail.com>
2024-10-24 18:17:35 +0700

    feat: Complete styling and layout for ListingAd component
    - Finalized styling and layout for the ListingAd display.
    - Added Font Inter for improved typography and readability.
    - Implemented logic to toggle description visibility when "Description" button is clicked, allowing users to easily view and hide additional details.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import { useState } from "react";
 import "./styles.css";
 
-export default function ListingAd({ pic, title, address, description }) {
+export default function ListingAd({
+  pic,
+  title,
+  address,
+  project_type,
+  year,
+  ownership_type,
+  psf_min,
+  psf_max,
+  subprice_label,
+  availabilities_label,
+  description,
+}) {
+  const [isDescriptionVisible, setDescriptionVisible] = useState(false);
+  const [isPhoneVisible, setPhoneVisible] = useState(false);
 
-  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
-  
-  const toggleDesctiption = () => {
-    setIsDescriptionVisible(!isDescriptionVisible);
-  }
+  const toggleDescription = () => {
+    setDescriptionVisible(!isDescriptionVisible);
+  };
+
+  const togglePhoneNumber = () => {
+    setPhoneVisible(!isPhoneVisible);
+  };
+
+  const hashPhoneNumber = (phoneNumber) => {
+    return `${phoneNumber.slice(0, 4)} XXXX`;
+  };
 
   return (
     <div className="App">
-      <img className="mainPic" width="300" height="500" src={pic} alt={title} />
-      <div className="mainContent">
-        <h1>{title}</h1>
-        <p className="address">{address}</p>
-        <button onClick={toggleDesctiption}>
-          {isDescriptionVisible ? 'Hide Description' : 'See Description'}
-        </button>
-        {isDescriptionVisible && <p className="description">{description}</p>}
+      <div className="card-container">
+        <div className="card">
+          <div className="back3d"></div>
+          <div className="label">Launching Soon</div>
+          <div className="img-container">
+            <div className="overlay"></div>
+            <img
+              className="mainPic"
+              width="300"
+              height="500"
+              src={pic}
+              alt={title}
+            />
+          </div>
+          <div className="mainContent">
+            <div className="title-content">
+              <img className="building-icon" src="/building-icon.svg" />
+              <div className="content">
+                <h1>{title}</h1>
+                <p className="address">{address}</p>
+              </div>
+              <p>
+                {project_type} · {year} · {ownership_type}
+              </p>
+              <p>{availabilities_label}</p>
+            </div>
+            <div className="price-section">
+              <p className="price-range">
+                ${psf_min} - ${psf_max} psf
+              </p>
+              <p className="sub-price">{subprice_label}</p>
+            </div>
+          </div>
+          <div className="description-section">
+            {isDescriptionVisible && (
+              <p className="description">{description}</p>
+            )}
+            <div className="btnDescription">
+              <button onClick={toggleDescription}>
+                {isDescriptionVisible ? "Hide Description" : "See Description"}
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
