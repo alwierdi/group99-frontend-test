e86295382975807cbfe37802a7f2c7458ff73ca9
alwierdi <alwi.erdi4@gmail.com>
2024-10-24 13:23:59 +0700

    feat: Implement button logic to toggle description visibility

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function ListingAd({ pic, title, address, description }) {
+
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+  
+  const toggleDesctiption = () => {
+    setIsDescriptionVisible(!isDescriptionVisible);
+  }
+
   return (
     <div className="App">
-      <img className="mainPic" width="300" height="500" src={pic} />
+      <img className="mainPic" width="300" height="500" src={pic} alt={title} />
       <div className="mainContent">
         <h1>{title}</h1>
         <p className="address">{address}</p>
-        <button>See description</button>
-        <p className="description">{description}</p>
+        <button onClick={toggleDesctiption}>
+          {isDescriptionVisible ? 'Hide Description' : 'See Description'}
+        </button>
+        {isDescriptionVisible && <p className="description">{description}</p>}
       </div>
     </div>
   );
